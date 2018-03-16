46b346529f042b5caa1be7ecd43377d838b61131
erfan.mehraban <erfan.mehraban@gmail.com>
2018-03-16 12:25:31 +0330

    ; added to end of queries

--- erd_schema_queries/1/Erfan/queries.sql
+++ erd_schema_queries/1/Erfan/queries.sql
@@ -1,9 +1,9 @@
 #1
 -- 2query way
-SELECT score FROM team WHERE team.name="esteghlal"
-SELECT score FROM team WHERE team.name="perspolis"
+SELECT score FROM team WHERE team.name="esteghlal";
+SELECT score FROM team WHERE team.name="perspolis";
 -- 1query way
-SELECT score FROM team WHERE team.name="esteghlal" or team.name="perspolis"
+SELECT score FROM team WHERE team.name="esteghlal" or team.name="perspolis";
 
 #2
 #saving id of saipa in view
@@ -20,70 +20,70 @@ SELECT * from game WHERE game.id =
         UNION
         SELECT game.id, game.guest_point as point FROM game,saipa WHERE game.host_team = saipa.id and game.guest_point > game.host_point) as bakht
     ORDER BY point DESC LIMIT 1
-)
+);
 
 #3
-SELECT * FROM team ORDER BY team.score LIMIT 1
+SELECT * FROM team ORDER BY team.score LIMIT 1;
 
 #4
-SELECT * FROM team ORDER BY team.score LIMIT 1 DESC
+SELECT * FROM team ORDER BY team.score LIMIT 1 DESC;
 
 #5
-SELECT team.coach FROM team ORDER BY team.score LIMIT 1
+SELECT team.coach FROM team ORDER BY team.score LIMIT 1;
 
 #6
 CREATE VIEW most_score AS
-(SELECT game.guest_team, game.host_team , SUM(game.host_point+game.guest_point) as points FROM game GROUP BY game.id ORDER BY points DESC LIMIT 1)
+(SELECT game.guest_team, game.host_team , SUM(game.host_point+game.guest_point) as points FROM game GROUP BY game.id ORDER BY points DESC LIMIT 1);
 
-SELECT team.* FROM team,most_score WHERE team.id=most_score.host_team or team.id=most_score.guest_team
+SELECT team.* FROM team,most_score WHERE team.id=most_score.host_team or team.id=most_score.guest_team;
 
 #7
 SELECT * FROM team WHERE team.id = 
-(SELECT team_id FROM
-# getting game wich has most score
-        (SELECT game.host_team as team_id, game.host_point as point FROM game,saipa WHERE game.guest_point < game.host_point
-        UNION
-        SELECT game.guest_team as team_id, game.guest_point as point FROM game,saipa WHERE game.guest_point > game.host_point) as bakht
-ORDER BY point LIMIT 1
-)
+    (SELECT team_id FROM
+    # getting game wich has most score
+            (SELECT game.host_team as team_id, game.host_point as point FROM game,saipa WHERE game.guest_point < game.host_point
+            UNION
+            SELECT game.guest_team as team_id, game.guest_point as point FROM game,saipa WHERE game.guest_point > game.host_point) as bakht
+    ORDER BY point LIMIT 1
+    );
 
 #8
 SELECT * FROM team WHERE team.id = 
-(SELECT team_id FROM
-# getting game wich has most score
-        (SELECT game.host_team as team_id, game.host_point as point FROM game,saipa WHERE game.guest_point < game.host_point
-        UNION
-        SELECT game.guest_team as team_id, game.guest_point as point FROM game,saipa WHERE game.guest_point > game.host_point) as bakht
-ORDER BY point DESC LIMIT 1
-)
+    (SELECT team_id FROM
+    # getting game wich has most score
+            (SELECT game.host_team as team_id, game.host_point as point FROM game,saipa WHERE game.guest_point < game.host_point
+            UNION
+            SELECT game.guest_team as team_id, game.guest_point as point FROM game,saipa WHERE game.guest_point > game.host_point) as bakht
+    ORDER BY point DESC LIMIT 1
+    );
 
 #9
 SELECT player.name FROM player WHERE player.id = 
 (
    SELECT team.captain_id FROM team WHERE team.name = "zob ahan"
-)
+);
 
 #10
 SELECT player.name from player WHERE player.injury=1 and player.team_id=
 (
     SELECT team.id FROM team WHERE team.name = "saih jamegan"
-)
+);
 
 #11
 SELECT player.name FROM player WHERE player.post="hamle" and player.team_id in
 (
     SELECT team.id FROM team WHERE team.in_Asia_league=1
-)
+);
 
 #12
-SELECT * FROM player WHERE player.in_national_team=1 ORDER BY age LIMIT 1
+SELECT * FROM player WHERE player.in_national_team=1 ORDER BY age LIMIT 1;
 
 #13
-SELECT player.name FROM player,team WHERE player.in_national_team=1 and player.team_id = team.id AND team.name="sepid rood"
+SELECT player.name FROM player,team WHERE player.in_national_team=1 and player.team_id = team.id AND team.name="sepid rood";
 
 #14
 # null in captian_team_id means player isnt captain
-SELECT player.name FROM player WHERE player.captian_team_id<>null
+SELECT player.name FROM player WHERE player.captian_team_id<>null;
 
 #15
-SELECT * FROM team WHERE team.in_Asia_league=1 AND team.city="tehran"
+SELECT * FROM team WHERE team.in_Asia_league=1 AND team.city="tehran";
--- erd_schema_queries/2/Erfan/queries.sql
+++ erd_schema_queries/2/Erfan/queries.sql
@@ -1,26 +1,26 @@
 #1
-SELECT department.addr from department,bank WHERE bank.name="mellat" AND bank.id=department.bank_id
+SELECT department.addr from department,bank WHERE bank.name="mellat" AND bank.id=department.bank_id;
 
 #2
-SELECT cu.name, cu.addr, cu.pn FROM customer cu, account ac WHERE ac.balance>1000000 AND ac.customer_id=cu.id
+SELECT cu.name, cu.addr, cu.pn FROM customer cu, account ac WHERE ac.balance>1000000 AND ac.customer_id=cu.id;
 
 #3
-SELECT loan.amount, customer.name FROM customer, loan WHERE customer.id=loan.customer_id ORDER BY loan.amount DESC LIMIT 1
+SELECT loan.amount, customer.name FROM customer, loan WHERE customer.id=loan.customer_id ORDER BY loan.amount DESC LIMIT 1;
 
 #4
-SELECT SUM(account.balance) FROM account, department, bank WHERE bank.name="refah" and bank.id=department.bank_id and account.department_id=department.id
+SELECT SUM(account.balance) FROM account, department, bank WHERE bank.name="refah" and bank.id=department.bank_id and account.department_id=department.id;
 
 #5
-SELECT SUM(account.balance) FROM account, customer WHERE account.customer_id=customer.id AND customer.name="irani"
+SELECT SUM(account.balance) FROM account, customer WHERE account.customer_id=customer.id AND customer.name="irani";
 
 #6
-SELECT department.addr, COUNT(customer.id) FROM department, bank, customer, account WHERE bank.name="keshavarzi" AND customer.id=account.customer_id AND account.department_id=department.id AND department.bank_id=bank.id GROUP BY department.id
+SELECT department.addr, COUNT(customer.id) FROM department, bank, customer, account WHERE bank.name="keshavarzi" AND customer.id=account.customer_id AND account.department_id=department.id AND department.bank_id=bank.id GROUP BY department.id;
 
 #7
-SELECT DISTINCT loan.type FROM loan, bank, department WHERE bank.id=department.bank_id AND bank.name="maskan" AND loan.department_id=department.id
+SELECT DISTINCT loan.type FROM loan, bank, department WHERE bank.id=department.bank_id AND bank.name="maskan" AND loan.department_id=department.id;
 
 #8
-SELECT COUNT(department.id) FROM bank, department WHERE bank.id = department.bank_id AND bank.name="ayande"
+SELECT COUNT(department.id) FROM bank, department WHERE bank.id = department.bank_id AND bank.name="ayande";
 
 #9
-SELECT customer.addr FROM bank, department, account, customer WHERE customer.id=account.customer_id AND account.department_id=department.id AND department.bank_id=bank.id AND bank.name="sepah" LIMIT 1
+SELECT customer.addr FROM bank, department, account, customer WHERE customer.id=account.customer_id AND account.department_id=department.id AND department.bank_id=bank.id AND bank.name="sepah" LIMIT 1;
